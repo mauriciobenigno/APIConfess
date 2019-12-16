5d0c4bf17913fae7b37148317f300366beeaf1ea
Mauricio Benigno <mauricio.soares@centi.com.br>
2019-12-16 17:09:48 -0300

    x

--- api.py
+++ api.py
@@ -70,8 +70,8 @@ def testeSQL():
 
 @app.route('/posts', methods=['POST'])
 def addConfess():
-	data = request.json
-	posts.append(data)
+	#data = request.json
+	#posts.append(data)
     '''cursor = db.cursor()
     query = """INSERT INTO postagens (TEXTO_POSTAGEM, COR_ID, NUMERO_CURTIDAS)
     VALUES (%s, %s, %s)"""
