15521dd3e8a60fa700412be281c4b16c3059e08c
Maurício Benigno <mauriciobenigno@hotmail.com>
2019-12-17 10:46:30 -0300

    Update api.py

--- api.py
+++ api.py
@@ -61,13 +61,13 @@ def addUser():
     #recebe o objeto json
     data = request.json
     #Adicionar usuário
-    '''query = "INSERT INTO heroku_5b193e052a7ad86.usuarios(APELIDO) " \
+    query = "INSERT INTO heroku_5b193e052a7ad86.usuarios(APELIDO) " \
                     "VALUES(%s)"
     args = (data['apelido'])    
     cursor = conn.cursor()
     cursor.execute(query, args)#executa o comando SQL
     data['id'] = cursor.lastrowid #extrai o ID que foi inserido e coloca no objeto recebido
-    conn.commit()#consolida as acoes no SQL'''
+    conn.commit()#consolida as acoes no SQL
     #retorna o objeto para o emitente com o ID atualizado
     return jsonify(data), 201
 
