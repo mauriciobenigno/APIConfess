0586b284261a4327fd45616378913bb2526a273f
Maurício Benigno <mauriciobenigno@hotmail.com>
2019-12-16 21:23:21 -0300

    Update api.py

--- api.py
+++ api.py
@@ -56,7 +56,7 @@ def getFavConfess():
 def foo():
     data = request.json
     cursor = db.cursor()
-    query = "INSERT INTO heroku_5b193e052a7ad86.postagens (TEXTO_POSTAGEM, COR_ID, NUMERO_CURTIDAS) VALUES ( 'maça', 7, 7)"
+    query = "INSERT INTO heroku_5b193e052a7ad86.postagens (TEXTO_POSTAGEM, COR_ID, NUMERO_CURTIDAS) VALUES ('Bacana', 5, 5)"
     #valores = (data['texto'], data['cor'], data['curtidas'])
     cursor.execute(query)
     data['id'] = cursor.lastrowid()
