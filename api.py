5603c6a001f22f2daf5ef1cf6a9bff54bd2b6872
Mauricio Benigno <mauriciobenigno@hotmail.com>
2020-09-18 22:06:36 -0300

    Adição de Get e Post

--- api.py
+++ api.py
@@ -25,27 +25,100 @@ atexit.register(fecharDB) #sempre que detectar que o terminal foi fechado, ele e
 # Flask 
 app = Flask(__name__)
 
-@app.route('/posts', methods=['POST'])
-def addPost():
+
+
+############# COISAS DE EMPRESA ####################
+
+@app.route('/empresas', methods=['POST'])
+def addEmpresa():
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
         #recebe o objeto json
         data = request.json
         #prepara a query para o sql
-        query = "INSERT INTO heroku_5b193e052a7ad86.postagens(TEXTO_POSTAGEM,COR_ID,USUARIO_ID,APELIDO) " \
-                            "VALUES(%s,%s,%s,%s)"
-        args = (data['texto'], data['cor'],data['autorid'],data['apelido'])
+        query = "INSERT INTO fdlc_empresa(fantasia,dtcadastro,descricao,end_lat,end_long) " \
+                            "VALUES(%s,%s,%s,%s,%s)"
+        args = (data['fantasia'], data['dtcadastro'],data['descricao'],data['end_lat'],data['end_long'])
         #posicionar o cursor no sql    
         cursor = conn.cursor()
         #executa o comando SQL
         cursor.execute(query, args)
         #extrai o ID que foi inserido
-        data['id'] = cursor.lastrowid
+        data['codempresa'] = cursor.lastrowid
         #consolida as acoes no SQL
         conn.commit()
         #retorna o objeto para o emitente com o ID bin
         conn.close()
         return jsonify(data), 201
+
+@app.route('/empresas/all', methods=['GET'])
+def getAllEmpresas():
+    conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
+    if conn.is_connected():
+        empresas = []
+        cursor = conn.cursor()
+        cursor.execute('SELECT codempresa,fantasia,descricao,end_lat,end_long FROM fdlc_empresa')
+        row = cursor.fetchone()
+        while row is not None:
+            data = {'codempresa': row[0],'fantasia': row[1],'descricao': row[2],'latitude': row[3],'longitude': row[4]}
+            empresas.append(data)
+            row = cursor.fetchone()
+        conn.close()
+        return jsonify(empresas), 200
+
+################ COISAS DE CAMPANHA #################
+
+@app.route('/campanhas', methods=['POST'])
+def addCampanha():
+    conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
+    if conn.is_connected():
+        #recebe o objeto json
+        data = request.json
+        #prepara a query para o sql
+        query = "INSERT INTO fdlc_campanha (nome,descricao,premio,regra,dt_inicio,dt_fim,meta,alcancado,incremento,pontuado,codempresa,codusuario) " \
+                            "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        args = (data['nome'], data['descricao'],data['premio'],data['regra'],data['dt_inicio'], data['dt_fim'],data['meta'],data['alcancado'],data['incremento'], data['pontuado'],data['codempresa'],data['codusuario'])
+        #posicionar o cursor no sql    
+        cursor = conn.cursor()
+        #executa o comando SQL
+        cursor.execute(query, args)
+        #extrai o ID que foi inserido
+        data['codcampanha'] = cursor.lastrowid
+        #consolida as acoes no SQL
+        conn.commit()
+        #retorna o objeto para o emitente com o ID bin
+        conn.close()
+        return jsonify(data), 201
+
+
+@app.route('/campanhas/all', methods=['GET'])
+def getAllCampanhas():
+    conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
+    if conn.is_connected():
+        empresas = []
+        cursor = conn.cursor()
+        cursor.execute('SELECT codcampanha,nome,descricao,premio,regra,dt_inicio,dt_fim,meta,alcancado,incremento,pontuado,codempresa,codusuario FROM fdlc_campanha')
+        row = cursor.fetchone()
+        while row is not None:
+            data = {'codcampanha': row[0],
+            'nome': row[1],
+            'descricao': row[2],
+            'premio': row[3],
+            'regra': row[4],
+            'dt_inicio': row[5],
+            'dt_fim': row[6],
+            'meta': row[7],
+            'alcancado': row[8],
+            'incremento': row[9],
+            'pontuado': row[10],
+            'codempresa': row[11],
+            'codusuario': row[12]}
+            empresas.append(data)
+            row = cursor.fetchone()
+        conn.close()
+        return jsonify(empresas), 200
+
+############ COISAS DE USUÁRIO ###############
 
 @app.route('/users', methods=['POST'])
 def addUser():
@@ -55,44 +128,49 @@ def addUser():
         data = request.json
         #Verifica se existe registro
         query = """SELECT CASE WHEN EXISTS (
-            SELECT * FROM  heroku_5b193e052a7ad86.usuarios a
-            WHERE  a.APELIDO = '{}'
+            SELECT * FROM  fdlc_usuarios a
+            WHERE  a.codusuario = '{}'
             )
             THEN 1 /* existe*/
             ELSE 0 /* nao existe*/
-            END AS resultado""".format(data['apelido'])
+            END AS resultado""".format(data['codusuario'])
         cursor = conn.cursor()
         cursor.execute(query)
         row = cursor.fetchone()
         if row[0] == 1: # Retorna usuário existente
             cursor = conn.cursor()
-            cursor.execute("SELECT * FROM heroku_5b193e052a7ad86.usuarios as a WHERE a.APELIDO ='"+data['apelido']+"' ;")
+            cursor.execute("SELECT codusuario,nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url FROM fdlc_usuario WHERE codusuario ='"+data['codusuario']+"' ;")
             row = cursor.fetchone()
-            data = {'id': row[0],'apelido': row[1]}
+            data = {'codusuario': row[0],
+            'nome': row[1],
+            'sobrenome': row[2],
+            'cpf': row[3],
+            'dtnascimento': row[4],
+            'email': row[5],
+            'telefone': row[6],
+            'estado': row[7],
+            'cidade': row[8],
+            'cep': row[9],
+            'image_url': row[10]}
             conn.close()
         else: #Adicionar usuário
-            query = "INSERT INTO heroku_5b193e052a7ad86.usuarios(APELIDO) VALUES('"+data['apelido']+"')"
+        #prepara a query para o sql
+            query = "INSERT INTO fdlc_usuario(nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url) " \
+                                "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            args = (data['nome'], data['sobrenome'],data['cpf'],data['dtnascimento'],data['email'], data['telefone'],data['estado'],data['cidade'],data['cep'], data['image_url'])
+            #posicionar o cursor no sql    
             cursor = conn.cursor()
-            cursor.execute(query)#executa o comando SQL
-            data['id'] = cursor.lastrowid #extrai o ID que foi inserido e coloca no objeto recebido
-            conn.commit()#consolida as acoes no SQL
+            #executa o comando SQL
+            cursor.execute(query, args)
+            #extrai o ID que foi inserido
+            data['codusuario'] = cursor.lastrowid
+            #consolida as acoes no SQL
+            conn.commit()
+            #retorna o objeto para o emitente com o ID bin
+            conn.close()
         #retorna o objeto para o emitente com o id
         return jsonify(data), 201
 
-@app.route('/empresas/all', methods=['GET'])
-def getAllEmpresas():
-    conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
-    if conn.is_connected():
-        empresas = []
-        cursor = conn.cursor()
-        cursor.execute('SELECT codempresa,fantasia,descricao,end_lat,end_long FROM empresa')
-        row = cursor.fetchone()
-        while row is not None:
-            data = {'codempresa': row[0],'fantasia': row[1],'descricao': row[2],'latitude': row[3],'longitude': row[4]}
-            empresas.append(data)
-            row = cursor.fetchone()
-        conn.close()
-        return jsonify(empresas), 200
 
 @app.route('/users/all', methods=['GET'])
 def getAllUsers():
@@ -100,54 +178,26 @@ def getAllUsers():
     if conn.is_connected():
         posts = []
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM heroku_5b193e052a7ad86.usuarios')
+        cursor.execute('SELECT codusuario,nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url FROM fdlc_usuario')
         row = cursor.fetchone()
         while row is not None:
-            data = {'id': row[0],'apelido': row[1]}
+            data = {'codusuario': row[0],
+            'nome': row[1],
+            'sobrenome': row[2],
+            'cpf': row[3],
+            'dtnascimento': row[4],
+            'email': row[5],
+            'telefone': row[6],
+            'estado': row[7],
+            'cidade': row[8],
+            'cep': row[9],
+            'image_url': row[10]}
             posts.append(data)
             row = cursor.fetchone()
         conn.close()
         return jsonify(posts), 200
 
-@app.route('/users/<apelido>', methods=['GET'])
-def getUser(apelido):
-    conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
-    if conn.is_connected():
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM heroku_5b193e052a7ad86.usuarios as a WHERE a.APELIDO ='"+apelido+"' ;")
-        row = cursor.fetchone()
-        data = {'id': row[0],'apelido': row[1]}
-        conn.close()
-        return jsonify(data), 200
-
-@app.route('/users/name/<id>', methods=['GET'])
-def getUserName(id):
-    conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
-    if conn.is_connected():
-        cursor = conn.cursor()
-        cursor.execute("SELECT a.APELIDO FROM heroku_5b193e052a7ad86.usuarios as a WHERE a.ID ='"+id+"' ;")
-        row = cursor.fetchone()
-        data = row[0]
-        conn.close()
-        return jsonify(data), 200
-
-@app.route('/users/posts/<apelido>', methods=['GET'])
-def getUserPosts(apelido):
-    conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
-    if conn.is_connected():
-        posts = []
-        cursor = conn.cursor()
-        query = """SELECT b.* FROM heroku_5b193e052a7ad86.usuarios a
-                INNER JOIN heroku_5b193e052a7ad86.postagens b ON b.USUARIO_ID = a.ID
-                WHERE a.APELIDO = '{}'""".format(apelido)
-        cursor.execute(query)
-        row = cursor.fetchone()
-        while row is not None:
-            data = {'id': row[0],'texto': row[1],'cor': row[2],'autorid': row[3],'apelido': row[4]}
-            posts.append(data)
-            row = cursor.fetchone()
-        conn.close()
-        return jsonify(posts), 200
+########### INIT E TESTE ##########
 
 @app.route('/teste', methods=['GET'])
 def testeSQL():
