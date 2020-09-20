0b3ecff54b38939fb6b67c46fdbc9542446a091e
Mauricio Benigno <mauriciobenigno@hotmail.com>
2020-09-20 16:12:02 -0300

    criação e update de usuário

--- api.py
+++ api.py
@@ -159,7 +159,7 @@ def getAllCampanhas():
 
 ############ COISAS DE USUÁRIO ###############
 
-@app.route('/users', methods=['POST'])
+@app.route('/user', methods=['PUT'])
 def addUser():
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
@@ -178,7 +178,7 @@ def addUser():
         row = cursor.fetchone()
         if row[0] == 1: # Retorna usuário existente
             cursor = conn.cursor()
-            cursor.execute("SELECT codusuario,nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url FROM fdlc_usuario WHERE codusuario ='"+data['codusuario']+"' ;")
+            cursor.execute("SELECT codusuario,nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url,status_cad FROM fdlc_usuario WHERE codusuario ='"+data['codusuario']+"' ;")
             row = cursor.fetchone()
             data = {'codusuario': row[0],
             'nome': row[1],
@@ -190,13 +190,14 @@ def addUser():
             'estado': row[7],
             'cidade': row[8],
             'cep': row[9],
-            'image_url': row[10]}
+            'image_url': row[10],
+            'status_cad': row[11]}
             conn.close()
         else: #Adicionar usuário
         #prepara a query para o sql
-            query = "INSERT INTO fdlc_usuario(nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url) " \
+            query = "INSERT INTO fdlc_usuario(nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url,status_cad) " \
                                 "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-            args = (data['nome'], data['sobrenome'],data['cpf'],data['dtnascimento'],data['email'], data['telefone'],data['estado'],data['cidade'],data['cep'], data['image_url'])
+            args = ('', '','','',data['email'], '','','','', '',false)
             #posicionar o cursor no sql    
             cursor = conn.cursor()
             #executa o comando SQL
@@ -210,8 +211,47 @@ def addUser():
         #retorna o objeto para o emitente com o id
         return jsonify(data), 201
 
+@app.route('/user', methods=['POST'])
+def updateUser():
+    conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
+    if conn.is_connected():
+        #recebe o objeto json
+        data = request.json
+        #Verifica se existe registro
+        query = """SELECT CASE WHEN EXISTS (
+            SELECT * FROM  fdlc_usuarios a
+            WHERE  a.codusuario = '{}'
+            )
+            THEN 1 /* existe*/
+            ELSE 0 /* nao existe*/
+            END AS resultado""".format(data['codusuario'])
+        cursor = conn.cursor()
+        cursor.execute(query)
+        row = cursor.fetchone()
+        if row[0] == 1: # Retorna usuário existente
+            cursor = conn.cursor()
+            queryUpdate = """ 
+            UPDATE fdlc_usuario SET nome = {},sobrenome = {},cpf={},dtnascimento={},email={},telefone={},estado={},cidade={},cep={},image_url={},status_cad={} WHERE codusuario = {}
+            """.format(data['nome'],data['sobrenome'],data['cpf'],data['dtnascimento'],data['email'],data['telefone'],data['estado'],data['cidade'],data['cep'],data['image_url'],data['status_cad'],data['codusuario'])
+            cursor.execute(queryUpdate)
+            row = cursor.fetchone()
+            data = {'codusuario': row[0],
+            'nome': row[1],
+            'sobrenome': row[2],
+            'cpf': row[3],
+            'dtnascimento': row[4],
+            'email': row[5],
+            'telefone': row[6],
+            'estado': row[7],
+            'cidade': row[8],
+            'cep': row[9],
+            'image_url': row[10],
+            'status_cad': row[11]}
+            conn.close()
+        else: jsonify(data), 401
+        return jsonify(data), 200
 
-@app.route('/users/all', methods=['GET'])
+@app.route('/user/all', methods=['GET'])
 def getAllUsers():
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
