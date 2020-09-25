a45237529b65b4db080ee8a475c1e639ecb6b597
Mauricio Benigno <mauriciobenigno@hotmail.com>
2020-09-25 20:17:52 -0300

    Update api.py

--- api.py
+++ api.py
@@ -96,7 +96,7 @@ def addEmpresa():
         return jsonify(data), 201
 
 @app.route('/empresas/all', methods=['GET'])
-#@check_for_token
+//@check_for_token
 def getAllEmpresas():
     print("chegou aqui")
     print(request.json)
@@ -141,10 +141,15 @@ def addPonto():
 def getAllPontos():
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
+        dataFromApp = request.json
         pontos = []
         cursor = conn.cursor()
-        cursor.execute('SELECT codponto,codusuario,codcampanha,pontuacao,dt_ponto FROM fdlc_ponto')
-        row = cursor.fetchone()
+        query ='''SELECT codponto,codusuario,codcampanha,pontuacao,dt_ponto FROM fdlc_ponto 
+                            INNER JOIN fdlc_usuario on fdlc_usuario.codusuario = fdlc_ponto.codusuario where fdlc_usuario.email like '{}'
+        '''.format(dataFromApp['email'])
+
+        cursor.execute(query)
+        row = cursor.fetchone(){}
         while row is not None:
             data = {'codponto': row[0],'codusuario': row[1],'codcampanha': row[2],'pontuacao': row[3],'dt_ponto': row[4]}
             pontos.append(data)
@@ -154,7 +159,7 @@ def getAllPontos():
 
 ################ COISAS DE CAMPANHA #################
 
-@app.route('/campanhas', methods=['POST'])
+@app.route('/campanhas', methods=['PUT'])
 def addCampanha():
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
@@ -177,13 +182,21 @@ def addCampanha():
         return jsonify(data), 201
 
 
-@app.route('/campanhas/all', methods=['GET'])
+@app.route('/campanhas/all', methods=['POST'])
 def getAllCampanhas():
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
         empresas = []
         cursor = conn.cursor()
-        cursor.execute('SELECT codcampanha,nome,descricao,premio,regra,dt_inicio,dt_fim,meta,alcancado,incremento,pontuado,codempresa,codusuario FROM fdlc_campanha')
+
+        dataFromApp = request.json
+        cursor = conn.cursor()
+        query ='''SELECT codcampanha,nome,descricao,premio,regra,dt_inicio,dt_fim,meta,alcancado,incremento,pontuado,codempresa,codusuario FROM fdlc_campanha 
+                            INNER JOIN fdlc_usuario on fdlc_usuario.codusuario = fdlc_campanha.codusuario where fdlc_usuario.email like '{}'
+        '''.format(dataFromApp['email'])
+
+        cursor.execute(query)
+
         row = cursor.fetchone()
         while row is not None:
             data = {'codcampanha': row[0],
@@ -206,14 +219,12 @@ def getAllCampanhas():
 
 ############ COISAS DE USUÁRIO ###############
 
-@app.route('/user', methods=['PUT'])
+@app.route('/user/register', methods=['PUT'])
 def addUser():
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
         #recebe o objeto json
         data = request.json
-        print("Request Json")
-        print(request.json)
         query = "INSERT INTO fdlc_usuario(nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url,status_cad) " \
                             "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         args = ('', '','','',data['email'], '','','','', '',False)
@@ -237,33 +248,34 @@ def updateUser():
         #recebe o objeto json
         data = request.json
         #Verifica se existe registro
-        query = """SELECT CASE WHEN EXISTS (
-            SELECT * FROM  fdlc_usuario a
-            WHERE  a.codusuario = '{}'
-            )
+        query = """SELECT CASE WHEN EXISTS ( SELECT * FROM  fdlc_usuario a WHERE  a.email = '{}')
             THEN 1 /* existe*/
             ELSE 0 /* nao existe*/
-            END AS resultado""".format(data['codusuario'])
+            END AS resultado""".format(data['email'])
         cursor = conn.cursor()
         cursor.execute(query)
         row = cursor.fetchone()
         if row[0] == 1: # Retorna usuário existente
             cursor = conn.cursor()
             queryUpdate = """ 
-            UPDATE fdlc_usuario SET nome = '{}',sobrenome = '{}',cpf='{}',dtnascimento='{}',email='{}',telefone='{}',estado='{}',cidade='{}',cep='{}',image_url='{}',status_cad={} WHERE codusuario = {}
-            """.format(data['nome'],'nada',data['cpf'],data['dtnascimento'],data['email'],data['telefone'],data['estado'],data['cidade'],data['cep'],data['image_url'],data['status_cad'],data['codusuario'])
+            UPDATE fdlc_usuario SET nome = '{}',sobrenome = '{}',cpf='{}',dtnascimento='{}',telefone='{}',estado='{}',cidade='{}',cep='{}',image_url='{}',status_cad={} WHERE email = {}
+            """.format(data['nome'],'nada',data['cpf'],data['dtnascimento'],data['telefone'],data['estado'],data['cidade'],data['cep'],data['image_url'],data['status_cad'],data['email'])
             cursor.execute(queryUpdate)
             conn.close()
         else: jsonify(data), 401
         return jsonify(data), 200
 
-@app.route('/user/all', methods=['GET'])
-def getAllUsers():
+@app.route('/user/login', methods=['POST'])
+def getUserFromEmail():
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
-        posts = []
+        dataFromApp = request.json
+
         cursor = conn.cursor()
-        cursor.execute('SELECT codusuario,nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url FROM fdlc_usuario')
+        query ='''SELECT codusuario,nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url FROM fdlc_usuario where fdlc_usuario.email like '{}'
+        '''.format(dataFromApp['email'])
+
+        cursor.execute(query)
         row = cursor.fetchone()
         while row is not None:
             data = {'codusuario': row[0],
@@ -277,10 +289,8 @@ def getAllUsers():
             'cidade': row[8],
             'cep': row[9],
             'image_url': row[10]}
-            posts.append(data)
-            row = cursor.fetchone()
-        conn.close()
-        return jsonify(posts), 200
+            conn.close()
+            return jsonify(posts), 200
 
 ########### INIT E TESTE ##########
 
