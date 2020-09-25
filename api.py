5ca92be2e142804e36fa291894d2a18df66e12a5
Mauricio Benigno <mauriciobenigno@hotmail.com>
2020-09-25 20:36:32 -0300

    Update api.py

--- api.py
+++ api.py
@@ -96,7 +96,6 @@ def addEmpresa():
         return jsonify(data), 201
 
 @app.route('/empresas/all', methods=['GET'])
-//@check_for_token
 def getAllEmpresas():
     print("chegou aqui")
     print(request.json)
