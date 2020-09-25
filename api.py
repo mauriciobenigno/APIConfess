4cf53dae2149c656e121432765db6c7e7be0629b
Mauricio Benigno <mauriciobenigno@hotmail.com>
2020-09-25 20:44:39 -0300

    Update api.py

--- api.py
+++ api.py
@@ -218,7 +218,7 @@ def getAllCampanhas():
 
 ############ COISAS DE USUÁRIO ###############
 
-@app.route('/user/register', methods=['PUT'])
+@app.route('/user/register', methods=['POST'])
 def addUser():
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
