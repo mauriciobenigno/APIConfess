6b46630233bcac748b59d0716b365a3c5fc660f2
Mauricio Benigno <mauriciobenigno@hotmail.com>
2020-09-25 22:30:20 -0300

    Update api.py

--- api.py
+++ api.py
@@ -267,6 +267,14 @@ def updateUser():
             """.format(data['nome'],'nada',data['cpf'],data['dtnascimento'],data['telefone'],data['estado'],data['cidade'],data['cep'],data['image_url'],data['status_cad'],data['email'])
             cursor.execute(queryUpdate)
             conn.commit()
+
+            query ='''SELECT codusuario,nome,sobrenome,cpf,dtnascimento,email,telefone,estado,cidade,cep,image_url FROM fdlc_usuario where fdlc_usuario.email like '{}'
+            '''.format(dataFromApp['email'])
+
+            row = cursor.fetchone()
+            while row is not None:
+                data = {'codusuario': row[0],'nome': row[1],'sobrenome': row[2],'cpf': row[3],'dtnascimento': row[4],'email': row[5],'telefone': row[6],'estado': row[7],'cidade': row[8],'cep': row[9], 'image_url': row[10]}
+
             conn.close()
         else: jsonify(data), 401
         return jsonify(data), 201
@@ -296,7 +304,7 @@ def getUserFromEmail():
             'cep': row[9],
             'image_url': row[10]}
             conn.close()
-            return jsonify(posts), 200
+            return jsonify(data), 200
 
 ########### INIT E TESTE ##########
 
