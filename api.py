fb6b7e6e7aeb971a2f3de122ebcbe8d527ad8ca3
Mauricio Benigno <mauriciobenigno@hotmail.com>
2020-09-28 20:59:19 -0300

    Update api.py

--- api.py
+++ api.py
@@ -74,6 +74,7 @@ def getToken():
 ############# COISAS DE LOCATION ####################
 @app.route('/locais', methods=['POST'])
 def getAllLocais():
+    print("CHEGOU REQUISICAO AQUI")
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
         dataFromApp = request.json
