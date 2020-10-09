d7d83575236594006943712a96b824118fce0421
Mauricio Benigno <mauriciobenigno@hotmail.com>
2020-10-09 14:04:10 -0300

    Update api.py

--- api.py
+++ api.py
@@ -101,7 +101,7 @@ def checkAndRegisterNumber():
                 row = cursor.fetchone()
 
             # Quando passar aqui,vai verificar se tem 3 meses desde a ultima atividade
-            date_time_obj = datetime.datetime.strptime(resultData['ultima_atividade'], '%Y-%m-%d %H:%M:%S.%f')
+            date_time_obj = datetime.strptime(resultData['ultima_atividade'], '%Y-%m-%d %H:%M:%S.%f')
             days = datetime.now() - date_time_obj
             if days > 90 : # Se tiver acima de 90 dias de diferença, faz update pra cadastrado = false
                 cursor = conn.cursor()
