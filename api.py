1273a9e594d3b7968ca5dddb6508f593f3cd793e
Mauricio Benigno <mauriciobenigno@hotmail.com>
2020-10-10 14:40:16 -0300

    Update api.py

--- api.py
+++ api.py
@@ -442,14 +442,14 @@ def updateUser():
         return jsonify(data), 201
 
 @app.route('/user/login', methods=['POST'])
-def getUserFromEmail():
+def getUserFromNumber():
     conn = mysql.connector.connect(host='us-cdbr-iron-east-05.cleardb.net',database='heroku_5b193e052a7ad86',user='bc3024c3520660',password='41d897e1')
     if conn.is_connected():
         dataFromApp = request.json
 
         cursor = conn.cursor()
         query ='''SELECT codusuario,nome,sobrenome,cpf,dtnascimento,email,telefone,logradouro,complemento,bairro,estado,cidade,cep,image_url,status_cad FROM fdlc_usuario where fdlc_usuario.telefone like '{}'
-        '''.format(dataFromApp['telefone'])
+        '''.format(dataFromApp['numero'])
 
         cursor.execute(query)
         row = cursor.fetchone()
